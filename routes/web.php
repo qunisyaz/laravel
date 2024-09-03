a9f38dc6a593688a790ef7b95f1c35c030c51306
qunisyaz <syazwinaauni31@gmail.com>
2024-09-03 15:31:32 +0800

    bengkel

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route:: get ('/',function(){
     return view('welcome');
+
 });
+
+Route::get('/cars', [App\Http\Controllers\CarController::class, 'index'])->name('cars.index');
+Route::get('/cars/create', [App\Http\Controllers\CarController::class, 'create'])->name('cars.create');
+Route::get('/cars/{id}/edit', [App\Http\Controllers\CarController::class, 'edit'])->name('cars.edit');
+Route::post('/cars/create', [App\Http\Controllers\CarController::class, 'store'])->name('cars.store');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
